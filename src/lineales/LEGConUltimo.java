e4105adce975dba16c494700370a79bfa6decc19
Diego <diego.garcia.santos22@estudiantes.uva.es>
2024-02-26 10:21:33 +0100

    Update LEGConUltimo.java

--- src/lineales/LEGConUltimo.java
+++ src/lineales/LEGConUltimo.java
@@ -4,4 +4,22 @@ import modelos.I_ListaConUltimo;
 
 public class LEGConUltimo<E> implements I_ListaConUltimo {
 
+    public void insertar(java.lang.Object x) {
+
+    }
+    public void insertarFinal(java.lang.Object elemento) {
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    public boolean eliminar(java.lang.Object elemento) {
+        return false;
+    }
+
+    public java.lang.Object recuperar(java.lang.Object elemento) {
+        return null;
+    }
 }
