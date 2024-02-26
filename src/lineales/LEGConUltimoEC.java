669ee6dbcf7d97fe74adb24f1b0722d581f86746
Diego <diego.garcia.santos22@estudiantes.uva.es>
2024-02-26 20:31:29 +0100

    comparables

---
+++ src/lineales/LEGConUltimoEC.java
@@ -0,0 +1,7 @@
+package lineales;
+//importa una libreria comparable
+import modelos.I_ListaConUltimoEC;
+
+public class LEGConUltimoEC<E extends Comparable<E>> extends LEGConUltimo<E> implements I_ListaConUltimoEC<E> {
+
+}
