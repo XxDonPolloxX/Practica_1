f909fce6058bcec08f91113e70b3c70e17683783
Dimitar Vasilev Vasilev <146078645+Miti33@users.noreply.github.com>
2024-02-27 09:10:22 +0100

    borrarMinimo

--- src/lineales/LEGConUltimoEC.java
+++ src/lineales/LEGConUltimoEC.java
@@ -6,6 +6,38 @@ import java.awt.*;
 
 
 public class LEGConUltimoEC<E extends Comparable<E>> extends LEGConUltimo<E> implements I_ListaConUltimoEC<E> {
+    public LEGConUltimoEC(){
+        super();
+    }
+    public void borrarMinimo() {
+    if(primero == null){
+            System.out.println("Lista vacía");
+        }
+        else{
+            NodoLEG<E> aux = primero;
+            NodoLEG<E> ant = null;
+            NodoLEG<E> min = primero;
+            NodoLEG<E> antMin = null;
+            while(aux != null){
+                if(aux.getDato().compareTo(min.getDato())<0){
+                    min = aux;
+                    antMin = ant;
+                }
+                ant = aux;
+                aux = aux.siguiente;
+            }
+            if(min == primero){
+                primero = primero.siguiente;
+            }
+            else{
+                antMin.siguiente = min.siguiente;
+            }
+            if(min == ultimo){
+                ultimo = antMin;
+            }
+        }
+    }
+
     public void moverCabeza() throws FalloEnOperacion{
         NodoLEG<E> aux;
         if(primero.getDato().compareTo(ultimo.getDato())<0){
