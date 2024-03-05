2a454ff0e69fad0c85fc26b0714d49386802f4db
Diego <diego.garcia.santos22@estudiantes.uva.es>
2024-03-05 16:50:46 +0100

    interfaz chukla

--- src/modelos/I_ListaConUltimoEC.java
+++ src/modelos/I_ListaConUltimoEC.java
@@ -2,7 +2,7 @@ package modelos;
 
 import lineales.FalloEnOperacion;
 
-public interface I_ListaConUltimoEC<E> {
+public interface I_ListaConUltimoEC<E extends Number> {
     public void borrarMinimo();
     public void moverCabeza() throws FalloEnOperacion;
     public void insertarCentinelas();
